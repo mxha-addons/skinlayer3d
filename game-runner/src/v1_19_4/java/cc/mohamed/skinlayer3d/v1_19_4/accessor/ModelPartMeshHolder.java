631feab68562d7a0baf9ab90d5803aed8a26f9d4
mxha39 <mohamed@mxha.de>
2025-11-23 15:07:00 +0100

    feat(game-runner): add 1.19.4

---
+++ game-runner/src/v1_19_4/java/cc/mohamed/skinlayer3d/v1_19_4/accessor/ModelPartMeshHolder.java
@@ -0,0 +1,10 @@
+package cc.mohamed.skinlayer3d.v1_19_4.accessor;
+
+import cc.mohamed.skinlayer3d.v1_19_4.render.Mesh;
+import cc.mohamed.skinlayer3d.v1_19_4.util.OffsetProvider;
+
+public interface ModelPartMeshHolder {
+
+    void skinlayer3d$attachMesh(Mesh mesh, OffsetProvider offsetProvider);
+
+}
