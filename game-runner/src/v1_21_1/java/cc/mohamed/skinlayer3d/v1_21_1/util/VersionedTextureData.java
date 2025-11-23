fee076185083481453de7673af822eaf2a0a10fd
mxha39 <mohamed@mxha.de>
2025-11-23 15:03:30 +0100

    feat(game-runner): add 1.21.1

---
+++ game-runner/src/v1_21_1/java/cc/mohamed/skinlayer3d/v1_21_1/util/VersionedTextureData.java
@@ -0,0 +1,44 @@
+package cc.mohamed.skinlayer3d.v1_21_1.util;
+
+import cc.mohamed.skinlayer3d.model.TextureData;
+import cc.mohamed.skinlayer3d.model.UV;
+import com.mojang.blaze3d.platform.NativeImage;
+
+public class VersionedTextureData implements TextureData {
+
+    private final NativeImage image;
+
+    public VersionedTextureData(NativeImage image) {
+        this.image = image;
+    }
+
+    @Override
+    public int width() {
+        return image.getWidth();
+    }
+
+    @Override
+    public int height() {
+        return image.getHeight();
+    }
+
+    @Override
+    public boolean isSolid(UV uv) {
+        if (uv.u() < 0 || uv.u() >= image.getWidth() || uv.v() < 0 || uv.v() >= image.getHeight()) {
+            return false;
+        }
+        int rgba = image.getPixelRGBA(uv.u(), uv.v());
+        int alpha = (rgba >> 24) & 0xFF;
+        return alpha == 255;
+    }
+
+    @Override
+    public boolean isPresent(UV uv) {
+        if (uv.u() < 0 || uv.u() >= image.getWidth() || uv.v() < 0 || uv.v() >= image.getHeight()) {
+            return false;
+        }
+        int rgba = image.getPixelRGBA(uv.u(), uv.v());
+        int alpha = (rgba >> 24) & 0xFF;
+        return alpha > 0;
+    }
+}
