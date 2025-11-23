8bede839741cf2d9b8bd9e2db18cf9e92067b6c0
mxha39 <mohamed@mxha.de>
2025-11-23 15:07:40 +0100

    feat(game-runner): add 1.17.1

---
+++ game-runner/src/v1_17_1/java/cc/mohamed/skinlayer3d/v1_17_1/accessor/HttpTextureAccessor.java
@@ -0,0 +1,10 @@
+package cc.mohamed.skinlayer3d.v1_17_1.accessor;
+
+import com.mojang.blaze3d.platform.NativeImage;
+import java.io.FileNotFoundException;
+
+public interface HttpTextureAccessor {
+
+    NativeImage skinlayer3d$getImage() throws FileNotFoundException;
+
+}
