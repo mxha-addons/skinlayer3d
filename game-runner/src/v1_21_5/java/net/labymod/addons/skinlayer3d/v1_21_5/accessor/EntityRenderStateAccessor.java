efc098053f85fd973385ff825d63a79d00706d3b
mxha39 <mohamed@mxha.de>
2025-12-04 00:10:19 +0100

    add 1.21.5

---
+++ game-runner/src/v1_21_5/java/net/labymod/addons/skinlayer3d/v1_21_5/accessor/EntityRenderStateAccessor.java
@@ -0,0 +1,11 @@
+package net.labymod.addons.skinlayer3d.v1_21_5.accessor;
+
+import net.minecraft.world.entity.Entity;
+
+public interface EntityRenderStateAccessor {
+
+    Entity skinlayer3d$getEntity();
+
+    void skinlayer3d$setEntity(Entity entity);
+
+}
